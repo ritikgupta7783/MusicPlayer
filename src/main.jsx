97f3b0a9e6a3aaaa092cca260de719980d15ecd4
Ritik Kumar Gupta <159267430+ritikgupta7783@users.noreply.github.com>
2025-06-21 21:25:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import PlayerContextProvider from './context/PlayerContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/GREY-MUSIC-PLAYER">
-   <PlayerContextProvider>
-     <App />
-   </PlayerContextProvider>
-   </BrowserRouter>
-    
-
+    <BrowserRouter>
+      <PlayerContextProvider>
+        <App />
+      </PlayerContextProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
