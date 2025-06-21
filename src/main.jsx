4008cce18b60ef68a12f3c981549cb3b691d6b03
ashishrao9983 <ashishrao9983@gmail.com>
2025-06-21 13:03:37 +0530

    new small changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import PlayerContextProvider from './context/PlayerContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename="/GREY-MUSIC-PLAYER">
+    <BrowserRouter basename="/GREY-MUSIC-PLAYER">
    <PlayerContextProvider>
      <App />
    </PlayerContextProvider>
-   </HashRouter>
+   </BrowserRouter>
     
 
   </StrictMode>,
